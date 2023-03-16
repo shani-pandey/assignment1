4752c6be3902b684dc2c04960c8d0f37990c5bc6
unknown <nikrairai19@gmail.com>
2023-03-16 23:36:25 +0530

    assignmet d

---
+++ src/app/modal-comp/modal-comp.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ModalCompComponent } from './modal-comp.component';
+
+describe('ModalCompComponent', () => {
+  let component: ModalCompComponent;
+  let fixture: ComponentFixture<ModalCompComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ModalCompComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ModalCompComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modal-comp/modal-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-comp',
+  templateUrl: './modal-comp.component.html',
+  styleUrls: ['./modal-comp.component.css']
+})
+export class ModalCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
